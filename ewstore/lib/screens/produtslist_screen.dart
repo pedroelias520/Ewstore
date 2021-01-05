d83025aa5c01310ef3e25ac6666a744b41e5458f
Pedro Elias <epedro520@gmail.com>
2021-01-04 22:43:05 -0300

    update: grid view as been created

--- ewstore/lib/screens/produtslist_screen.dart
+++ ewstore/lib/screens/produtslist_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ewstore/commons/drawer_common/Drawer.dart';
@@ -131,12 +130,7 @@ class _ProductListState extends State<ProductList> {
               alignment: Alignment(0.0, 1.0),
               child: Container(
                   height: screenHeight - (screenHeight / 3),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white, width: 5),
-                  ),
-                  child: ListView.builder(itemBuilder: (context, index) {
-                    return _ProductsCard(context);
-                  })),
+                  child: _ProductsCard(context)),
             )
           ],
         ),
@@ -147,84 +141,111 @@ class _ProductListState extends State<ProductList> {
   Widget _ProductsCard(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
+    String personUrl =
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficon-icons.com%2Ficon%2Fperson%2F110935&psig=AOvVaw3l_rLw87GU1nu1E_1IGzPS&ust=1609892769269000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiCt_zDg-4CFQAAAAAdAAAAABAO';
+    List<DocumentSnapshot> ProductList;
     return StreamBuilder<QuerySnapshot>(
-        stream: Firestore.instance
-            .collection('Produts')            
-            .snapshots(),
+        stream: Firestore.instance.collection('Produts').snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) return new Text('ERROR: ${snapshot.error}');
           switch (snapshot.connectionState) {
             case ConnectionState.waiting:
               return new Center(
                 child: Column(
-                  children: [
-                    Text(
-                      'Carregando produtos',
-                      style: TextStyle(fontFamily: 'MontserratB', fontSize: 25, color: Colors.white)
-                    ),
-                    CircularProgressIndicator()
-                  ],
+                  children: [CircularProgressIndicator()],
                 ),
               );
-            default:              
-              return new SizedBox(
-                  height: screenHeight / 4,
-                  child: ListView(
-                    children: snapshot.data.documents
-                        .map((DocumentSnapshot document) {
-                          debugPrint(document['name'].toString());
-                      return GestureDetector(                        
-                        child: Card(                                                                              
-                          margin: EdgeInsets.all(5),
-                          color: Colors.white,
-                          child: Container(
-                            width: screenWidth / 3,
-                            height: screenHeight / 8,
-                            child: Column(
-                              children: <Widget>[
+            default:
+              ProductList = snapshot.data.documents;
+              return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, mainAxisSpacing: 0 ,crossAxisSpacing: 0),
+                  itemCount: ProductList.length ,
+                  padding: EdgeInsets.only(left: 50, right: 50, top: 30),
+                  itemBuilder: (BuildContext context, int index) {
+                    return SizedBox(                                       
+                      child: GestureDetector(                          
+                          onTap: () {},
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            color: Colors.transparent,
+                            child: Stack(                              
+                              children: [
                                 Container(
+                                  height: screenHeight / 4,
                                   decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        image: new NetworkImage(
-                                            document['image'].toString()),
-                                        repeat: ImageRepeat.noRepeat,
-                                        fit: BoxFit.cover),
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(20)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        bottom: 10, left: 10, right: 10),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Align(
+                                          alignment: Alignment(-1.0, 0.0),
+                                          child: AutoSizeText(
+                                            ProductList[index]['name'],
+                                            style: TextStyle(
+                                                fontFamily: 'MontserratB'),
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(top: 5),
+                                          child: Stack(
+                                            alignment: Alignment.center,
+                                            children: [
+                                              Align(
+                                                alignment: Alignment(-1.0, 0.0),
+                                                child: Text(
+                                                  "${ProductList[index]['price']} Rs",
+                                                  style: TextStyle(
+                                                      fontFamily:
+                                                          'MontserratM'),
+                                                ),
+                                              ),
+                                              Align(
+                                                alignment: Alignment(1.0, 0.0),
+                                                child: Container(
+                                                  height: 35,
+                                                  width: 35,
+                                                  child: RaisedButton(
+                                                      shape:
+                                                          RoundedRectangleBorder(
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          80.0)),
+                                                      child: Text('+',style: TextStyle(fontFamily: 'MontserratM',color: Colors.white,fontSize: 20),textAlign: TextAlign.center,),
+                                                      color: Color.fromRGBO(
+                                                          254, 207, 162, 1),
+                                                      onPressed: () {}),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
                                   ),
                                 ),
-                                Expanded(
-                                    flex: 8,
-                                    child: Container(
-                                      decoration:
-                                          BoxDecoration(color: Colors.white),
-                                      child: Stack(
-                                        children: [
-                                          Align(
-                                            child: AutoSizeText(
-                                              document['name'].toString(),
-                                              style: TextStyle(
-                                                  fontFamily: 'MontserratB',
-                                                  fontSize: 15),
-                                            ),
-                                          ),
-                                          Align(
-                                            child: AutoSizeText(
-                                                document['price'].toString()),
-                                          ),
-                                          RaisedButton(
-                                            color: Color.fromRGBO(254, 207, 162, 1),
-                                            onPressed: (){},
-                                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)))                                            
-                                        ],
-                                      ),
-                                    )),
+                                Align(
+                                  alignment: Alignment(0.0, -5.5),
+                                  child: Container(
+                                      height: 120,
+                                      width: 120,
+                                      child: Image.network(
+                                        ProductList[index]['image'],
+                                        scale: 1.0,
+                                      )),
+                                )
                               ],
                             ),
-                          ),
-                        ),
-                        onTap: () {},
-                      );
-                    }).toList()),
+                          )),
                     );
+                  });
           }
         });
   }
