cfd837f9b14694c3a0e3aafa37ac30b5aea796a0
Pedro Elias <epedro520@gmail.com>
2021-02-17 23:08:22 -0300

    update: card addded

--- ewstore/lib/screens/form_screen.dart
+++ ewstore/lib/screens/form_screen.dart
@@ -8,9 +8,22 @@ class FormScreen extends StatefulWidget {
 class _FormScreenState extends State<FormScreen> {
   @override
   Widget build(BuildContext context) {
-    return Stack(      
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Stack(
       children: [
-        
+        Align(
+          alignment: Alignment(0.0, 1.0),
+          child: Container(            
+            height: screenHeight / 2,
+            width: screenWidth - 20,
+            child: Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topLeft: Radius.circular(40),topRight: Radius.circular(40))),
+              color: Colors.white38,
+              child: Text("Cartão"),
+            ),
+          ),
+        )
       ],
     );
   }
